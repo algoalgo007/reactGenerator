61e4b09e9104956df59173911e6bf2fa24f97991
YoonKiBum <ykbsuper@naver.com>
2024-07-30 22:17:25 +0900

    feature: remove test

--- src/test/java/com/bgspro/componentGenerator/ComponentGeneratorApplicationTests.java
+++ src/test/java/com/bgspro/componentGenerator/ComponentGeneratorApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bgspro.componentGenerator;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ComponentGeneratorApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.bgspro.componentGenerator;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ComponentGeneratorApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
